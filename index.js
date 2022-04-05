7401774d462dbaab32ac8c28c2c7b75d1c0566bc
Zicheng.S <zichengshan@gmail.com>
2022-04-04 20:35:05 -0700

    set up flexbox and some css

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+
