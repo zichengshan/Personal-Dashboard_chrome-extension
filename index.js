f42d6573c6663ffc80c13f975de5da3f21f85efe
Zicheng.S <zichengshan@gmail.com>
2022-04-04 20:16:35 -0700

    Add author info

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature", {method:"GET"})
     .then(res => res.json())
     .then(data => {
+        console.log(data)
         document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
     })
-
-
-// links download
