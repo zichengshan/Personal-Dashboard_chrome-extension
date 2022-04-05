46b90419f953b25ee3cba270f0988a51ead20f3a
Zicheng.S <zichengshan@gmail.com>
2022-04-04 21:15:36 -0700

    promise rejection => backup background image

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+    .catch(err => {
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1446034295857-c39f8844fad4?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDkxMzIwMTI&ixlib=rb-1.2.1&q=85)`
+        document.getElementById("author").textContent = `By: Vadim Sherbakov`
+    })
 
