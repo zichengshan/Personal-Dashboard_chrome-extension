d6552ebe157d1b3a47855c69f035478f647282e4
Zicheng.S <zichengshan@gmail.com>
2022-04-04 23:28:19 -0700

    add temp and city

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ navigator.geolocation.getCurrentPosition((position) => {
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML += `
                 <img src=${iconUrl}>
+                <p>${Math.round(data.main.temp)}°C</p>
+                <p>${data.name}</p>
             `
         })
         .catch(err => {console.error(err)})
